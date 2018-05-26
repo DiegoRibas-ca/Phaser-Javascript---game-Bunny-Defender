407efbafb154a5a8ffa0912cd89f431c9ab315d4
DiegoRibas-ca <diegoribas.ca@gmail.com>
2018-05-26 19:33:02 -0400

    bunnies moviments using phaser utilities as tween.

--- Game.js
+++ Game.js
@@ -24,7 +24,32 @@ BunnyDefender.Game.prototype = {
             b.animations.add('Rest', this.game.math.numberArray(1, 58));
             b.animations.add('Walk', this.game.math.numberArray(68, 107));
             b.animations.play('Rest', 24, true);
+            this.assignBunnyMovement(b);
         }
+    },
+
+    assignBunnyMovement: function (b) {
+        bposition = Math.floor(this.rnd.realInRange(50, this.world.width - 50));
+        bdelay = this.rnd.integerInRange(2000, 6000);
+        if (bposition < b.x) {
+            b.scale.x = 1;
+        } else {
+            b.scale.x = -1;
+        }
+        t = this.add.tween(b).to({ x: bposition }, 3500, Phaser.Easing.Quadratic.InOut, true, bdelay); //properties, duration, ease, autoStart, delay
+        t.onStart.add(this.startBunny, this);
+        t.onComplete.add(this.stopBunny, this);
+    },
+
+    startBunny: function (b) {
+        b.animations.stop('Rest');
+        b.animations.play('Walk', 24, true);
+    },
+
+    stopBunny: function (b) {
+        b.animations.stop('Walk');
+        b.animations.play('Rest', 24, true);
+        this.assignBunnyMovement(b);
     },
 
     update: function() {}
